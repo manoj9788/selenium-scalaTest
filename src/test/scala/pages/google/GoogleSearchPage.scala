d556ab81694f35f2ba838637096e95981f9cdc7b
Manoj Kumar <manoj9788@gmail.com>
2020-04-17 18:09:58 +0530

    remove unused pageFactory

--- src/test/scala/pages/google/GoogleSearchPage.scala
+++ src/test/scala/pages/google/GoogleSearchPage.scala
@@ -6,8 +6,6 @@ class GoogleSearchPage(private var driver: WebDriver) {
 
   private var searchField: By = By.name("q")
 
-  //PageFactory.initElements(driver, this)
-
   def searchFor(searchTerm: String): Unit ={
 
     driver.findElement(searchField).sendKeys(searchTerm + Keys.RETURN)
